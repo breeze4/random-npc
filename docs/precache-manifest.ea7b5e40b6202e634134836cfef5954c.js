429fd6712e80f06fdc95ba1f6799a9bc0ea231c2
Matt Bailey <ambiturner0@gmail.com>
2019-02-25 07:58:56 -0800

    remove gh-pages, add /docs

---
+++ docs/precache-manifest.ea7b5e40b6202e634134836cfef5954c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd8d17c453618af5163a",
+    "url": "/random-npc/static/js/runtime~main.cd8d17c4.js"
+  },
+  {
+    "revision": "1061f708508197cfc072",
+    "url": "/random-npc/static/js/main.1061f708.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/random-npc/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "1061f708508197cfc072",
+    "url": "/random-npc/static/css/main.5e614e82.chunk.css"
+  },
+  {
+    "revision": "c733b09cf555e8313559459e98a0c78d",
+    "url": "/random-npc/index.html"
+  }
+];
